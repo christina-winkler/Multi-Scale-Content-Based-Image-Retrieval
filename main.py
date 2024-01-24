2c5559fefe81931976a178e180ea18bf613f3064
ge26yim <ce.winkler@tum.de>
2024-01-24 15:45:58 +0100

    erer

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ sys.path.append("../../")
 
 import argparse
 import torch
+from torchvision import datasets, transforms,models
 
 # Dataset loading
 from datasets import dataloading
@@ -54,14 +55,17 @@ def main(args):
 
     # Build name of current model
     if args.modelname is None:
-        args.modelname = "{}_{}_bsz{}_K{}_L{}_lr{:.4f}_s{}".format(args.modeltype, args.trainset,
-                                                                   args.bsz, args.K, args.L,
-                                                                   args.lr, args.s)
-    if args.train:
-        # load data
-        train_loader, valid_loader, test_loader, args = dataloading.load_data(args)
-        in_channels = next(iter(test_loader))[0].shape[0]
-        height, width = next(iter(train_loader))[0].shape[1], next(iter(train_loader))[0].shape[2]
+        args.modelname = "{}_{}_bsz{}_lr{:.4f}".format(args.modeltype, args.trainset,
+                                                                   args.bsz, args.lr)
+
+    # load data
+    train_loader, valid_loader, test_loader, args = dataloading.load_data(args)
+    in_channels = next(iter(test_loader))[0].shape[0]
+    height, width = next(iter(train_loader))[0].shape[1], next(iter(train_loader))[0].shape[2]
+
+    if args.modeltype == 'densenet161':
+        model = models.densenet161(pretrained=True)
+        trainer.train(args, train_loader, valid_loader, model, device='cpu')
 
     print("Start training {} on {}:".format(args.modeltype, args.trainset))
 
@@ -71,8 +75,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # train configs
-    parser.add_argument("--modeltype", type=str, default="srflow",
-                        help="Specify modeltype you would like to train [srflow, cdiff, stflow].")
+    parser.add_argument("--modeltype", type=str, default="densenet161",
+                        help="Specify modeltype you would like to train.")
     parser.add_argument("--model_path", type=str, default="runs/",
                         help="Directory where models are saved.")
     parser.add_argument("--modelname", type=str, default=None,
@@ -95,19 +99,10 @@ if __name__ == "__main__":
                         help="Disable scale in coupling layers.")
     parser.add_argument("--testmode", action="store_true",
                         help="Model run on test set.")
-    parser.add_argument("--train", action="store_true",
-                        help="If model should be trained.")
     parser.add_argument("--resume", action="store_true",
                         help="If training should be resumed.")
 
     # hyperparameters
-    parser.add_argument("--nbits", type=int, default=8,
-                        help="Images converted to n-bit representations.")
-    parser.add_argument("--s", type=int, default=2, help="Upscaling factor.")
-    parser.add_argument("--gauss_steps", type=int, default=1000,
-                        help="Number of gaussianization steps in diffusion process.")
-    parser.add_argument("--noise_sched", type=str, default='cosine',
-                        help="Type of noise schedule defining variance of noise that is added to the data in the diffusion process.")
     parser.add_argument("--crop_size", type=int, default=500,
                         help="Crop size when random cropping is applied.")
     parser.add_argument("--patch_size", type=int, default=500,
@@ -117,17 +112,10 @@ if __name__ == "__main__":
                         help="learning rate")
     parser.add_argument("--filter_size", type=int, default=512//2,
                         help="filter size NN in Affine Coupling Layer")
-    parser.add_argument("--L", type=int, default=3, help="# of levels")
-    parser.add_argument("--K", type=int, default=2,
-                        help="# of flow steps, i.e. model depth")
     parser.add_argument("--nb", type=int, default=16,
                         help="# of residual-in-residual blocks LR network.")
     parser.add_argument("--condch", type=int, default=128//8,
                         help="# of residual-in-residual blocks in LR network.")
-    parser.add_argument("--linear_start", type=float, default=1e-6,
-                        help="Minimum value of the linear schedule (for diffusion model).")
-    parser.add_argument("--linear_end", type=float, default=1e-2,
-                        help="Maximum value of the linear schedule (for diffusion model).")
 
     # data
     parser.add_argument("--datadir", type=str, default="/home/christina/Documents/CV_ML_Assignment/datasets/")
